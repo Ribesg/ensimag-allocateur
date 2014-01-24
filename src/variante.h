190b2895f6af1aad7fe90dbc0ba75df43da9c67a
Gael <ribesg@yahoo.fr>
2014-01-24 15:00:32 +0100

    Variante choisie

---
+++ src/variante.h
@@ -0,0 +1,10 @@
+#ifndef VARIANTE_H
+#define VARIANTE_H
+
+#define LOGINS cadice;ribesg
+#define SUJETSHA 0b5ec46d950769719c235aaeab3bd41dddd39a7afdd1b952a2dd1b605114b0b848611ba6e7d6ee850b8c0fe60d22e6f6c21dcbbf1288bee4ce96978c1dc73de0
+
+#define SUJET 1
+
+
+#endif
